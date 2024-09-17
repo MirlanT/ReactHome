3f2c46a596f556a7ba75eecc5794cfe5d557d04d
MirlanT <107634023+MirlanT@users.noreply.github.com>
2024-09-17 14:31:53 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Header = () =>{
+    return (
+        <header className='header'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2fRG5QVb8rTefzrf6y9C8ItQit7boBr87Iw&s' alt='monkey'></img>
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Main = () =>{
+    return(
+        <div className='main'>
+        <img alt='logo' src='https://yaves.ru/_next/image?url=https%3A%2F%2Fyaves.ru%2Fbackend%2Fstorage%2Ffiles%2Fblogs%2F324%2F1686834447orcqMRl1zo0fcFZ0.jpg&w=3840&q=75'></img>
+        <div>content</div>
+        <div>content</div>
+        <div>content</div>
+        <div>content</div>
+        <div>content</div>
+      </div>
+    )
+}
+export default Main

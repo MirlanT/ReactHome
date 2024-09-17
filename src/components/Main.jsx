b1426d8195c5bc2fd6edb9e2782fb1235ba519f5
MirlanT <107634023+MirlanT@users.noreply.github.com>
2024-09-17 18:12:33 +0600

    add commponents

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,18 @@
 import React from "react"
-
+import classes from'./Main.module.css';
 const Main = () =>{
     return(
-        <div className='main'>
+        <div className={classes.main}>
         <img alt='logo' src='https://yaves.ru/_next/image?url=https%3A%2F%2Fyaves.ru%2Fbackend%2Fstorage%2Ffiles%2Fblogs%2F324%2F1686834447orcqMRl1zo0fcFZ0.jpg&w=3840&q=75'></img>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
+
+        <div className={classes.content}>
+          <div className={classes.content}>content</div>
+          <div className={classes.content}>content</div>
+          <div className={classes.content}>content</div>
+          <div className={classes.content}>content</div>
+          <div className={classes.content}>content</div>
+          <div className={classes.content}>content</div>
+        </div>
       </div>
     )
 }
